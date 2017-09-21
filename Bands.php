18e15c84ed43e35c0d21ffe29a860781b915303f
LucaMarini <32158335+LucaMarini@users.noreply.github.com>
2017-09-21 11:34:22 +0200

    Update Bands.php

--- Bands.php
+++ Bands.php
@@ -1,28 +1,25 @@
 <?php
-$longevita =array(0,0,0,0);
+$longevita = array (0,0,0,0);
 $maxLong = 0;
 $minLong = 99999;
+$count = 0;
 //Pink Floyd
 $pinkFloyd["nome"]= "Pink Floyd";
-$pinkFloyd["nazione"]= "Inghilterra";
 $pinkFloyd["genere"]= "progressive rock";
 $pinkFloyd["annoI"]= 1965;
 $pinkFloyd["annoF"]= 2005;
 //AC/DC
 $acdc["nome"]= "AC/DC";
-$acdc["nazione"]= "Australia";
 $acdc["genere"]= "hard rock";
 $acdc["annoI"]= 1973;
 $acdc["annoF"]=2017;
 //Beatles
 $beatles["nome"]= "Beatles";
-$beatles["nazione"]= "Inghilterra";
 $beatles["genere"]= "beat";
 $beatles["annoI"]= 1960;
 $beatles["annoF"]=1970;
 //Led Zeppelin
 $led["nome"]="Led Zeppelin";
-$led["nazione"]="Inghilterra";
 $led["genere"]="blues rock";
 $led["annoI"]=1965;
 $led["annoF"]=1980;
@@ -31,6 +28,7 @@ $longevita[0]=$pinkFloyd["annoF"]-$pinkFloyd["annoI"];
 $longevita[1]=$acdc["annoF"]-$acdc["annoI"];
 $longevita[2]=$beatles["annoF"]-$beatles["annoI"];
 $longevita[3]=$led["annoF"]-$led["annoI"];
+echo "ANALIZZIAMO LE INFORMAZIONI DI 4 BAND: PINK FLOYD, AC/DC, BEATLES E ROLLING STONES \n\n\n";
 for($i = 0 ; $i<4; $i++)
 {
 	if($maxLong<$longevita[$i])
@@ -75,9 +73,17 @@ else if($minLong==$longevita[2])
 {
 	$minName="Beatles";
 }
-else
+else if($minLong==$longevita[3])
 {
 	$minName="Led Zeppelin";
 }
-echo "La band meno longeva sono i ".$minName.", attivi per ".$minLong." anni.";
+echo "La band meno longeva sono i ".$minName.", attivi per ".$minLong." anni.\n";
+$sum = $longevita[0]+$longevita[1]+$longevita[2]+$longevita[3];
+$media = $sum / 4;
+echo "La media del periodo di attività delle 4 band è ".$media." anni.\n\n";
+echo "Andiamo ora a vedere il principale genere musicale per ogni band: \n";
+echo $pinkFloyd["nome"].": ".$pinkFloyd["genere"]."\n";
+echo $acdc["nome"].": ".$acdc["genere"]."\n";
+echo $beatles["nome"].": ".$beatles["genere"]."\n";
+echo $led["nome"].": ".$led["genere"]."\n";
 ?>
